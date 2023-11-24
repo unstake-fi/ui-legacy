3e187bf00a15ae4602051c04101ae76131c18a72
slangdog <hello@brettmeyer.net>
2023-11-24 11:01:39 +0000

    wrap tx hash

--- src/App.tsx
+++ src/App.tsx
@@ -302,11 +302,17 @@ const Result: FC<{
         role="alert"
       >
         <strong className="font-bold mr-1 text-teal-500">Success:</strong>
-        <span className="block sm:inline mr-1">{result.transactionHash}</span>
+        <span
+          className="block sm:inline mr-1 text-left"
+          style={{ wordBreak: "break-all" }}
+        >
+          {result.transactionHash}
+        </span>
         <svg
           className="fill-current opacity-75 h-4 w-4 ml-auto"
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
+          style={{ flexShrink: 0 }}
         >
           <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
         </svg>
